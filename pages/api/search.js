359c47d3fe9ab6530a54488e08d4cba62ebf9cff
alopezo <8TPFdVPnehjv4d>
2023-05-28 14:30:40 -0300

    Multilanguage version

--- pages/api/search.js
+++ pages/api/search.js
@@ -3,8 +3,25 @@ import cors from '../../middleware/cors';
 import fetch from 'node-fetch'
 
 export default async function handler(req, res) {
-    const { term } = req.query;
-    const response = await fetch(`https://snowstorm.ihtsdotools.org/fhir/ValueSet/$expand?filter=${term}&offset=0&count=1&language=en&url=http%3A%2F%2Fsnomed.info%2Fsct%2F900000000000207008%3Ffhir_vs&_format=json`)
+    console.log(req.query);
+    const term = req.query.term;
+    const modulesMap = {
+        en: '900000000000207008',
+        es: '449081005',
+        da: '554471000005108',
+        sv: '45991000052106', 
+        fr: '11000241103',
+        nl: '11000146104'
+    }
+    let language = 'en';
+    let moduleId = '900000000000207008';
+    if (req.query.language && modulesMap[language]) {
+        language = req.query.language;
+        moduleId = modulesMap[language];
+    }
+    const url = `https://snowstorm.ihtsdotools.org/fhir/ValueSet/$expand?filter=${term}&offset=0&count=1&language=${language}&displayLanguage=${language}&url=http%3A%2F%2Fsnomed.info%2Fsct%2F${moduleId}%3Ffhir_vs&_format=json`;
+    console.log(url);
+    const response = await fetch(url)
     const data = await response.json();
     res.status(200).json(data);
 }
