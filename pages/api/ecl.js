bb0543a644d3ed44afc3911db6d893c5cdde3dbd
alopezo <8TPFdVPnehjv4d>
2023-05-31 08:58:17 -0300

    Adding ECL endpoint

---
+++ pages/api/ecl.js
@@ -0,0 +1,25 @@
+// pages/api/search.js
+import cors from '../../middleware/cors';
+import fetch from 'node-fetch'
+
+export default async function handler(req, res) {
+    const ecl = req.query.ecl;
+    const modulesMap = {
+        en: '900000000000207008',
+        es: '449081005',
+        da: '554471000005108',
+        sv: '45991000052106', 
+        fr: '11000241103',
+        nl: '11000146104'
+    }
+    let language = 'en';
+    let moduleId = '900000000000207008';
+    if (req.query.language && modulesMap[language]) {
+        language = req.query.language;
+        moduleId = modulesMap[language];
+    }
+    const url = `https://snowstorm.ihtsdotools.org/fhir/ValueSet/$expand?offset=0&count=10&language=${language}&displayLanguage=${language}&url=http%3A%2F%2Fsnomed.info%2Fsct%2F${moduleId}%3Ffhir_vs=ecl/${ecl}&_format=json`;
+    const response = await fetch(url)
+    const data = await response.json();
+    res.status(200).json(data);
+}
